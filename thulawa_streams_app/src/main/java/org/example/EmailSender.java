505e80be3810d8267f51853ab1dccd48dd605a8b
Keshan <janindupathi@gmail.com>
2025-03-19 00:22:49 +0530

    Updates

--- thulawa_streams_app/src/main/java/org/example/EmailSender.java
+++ thulawa_streams_app/src/main/java/org/example/EmailSender.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.mail.*;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
@@ -7,16 +8,17 @@ import jakarta.mail.internet.MimeMessage;
 import java.util.Properties;
 
 public class EmailSender {
-    private static final String SMTP_HOST = "smtp.gmail.com";
-    private static final String SMTP_USER = " ";
-    private static final String SMTP_PASS = " ";
+    private static final String SMTP_HOST = Dotenv.load().get("SMTP_HOST");
+    private static final String SMTP_USER = Dotenv.load().get("SMTP_USER");
+    private static final String SMTP_PASS = Dotenv.load().get("SMTP_PASS");
+    private static final String SMTP_PORT = Dotenv.load().get("SMTP_PORT");
 
     public static void sendEmail(String to, String subject, String body) {
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", SMTP_HOST);
-        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.port", SMTP_PORT);
 
         Session session = Session.getInstance(props, new Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
